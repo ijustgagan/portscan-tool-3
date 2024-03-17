4f9a9df21ddc2ea3bc39237690c4be095982960b
ijustgagan <rathoregagan726@gmail.com>
2024-03-18 00:59:36 +0530

    updated file with readme

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # portscan-tool-3
 it is a cyber security tool 
+
+Sure, here's a README file for your tool with installation instructions and usage guidelines:
+
+---
+
+# Web Security Toolkit
+
+## Description
+This toolkit provides various tools for web security testing, including WordPress enumeration, XSS (Cross-Site Scripting) search, SQL injection scanning, and port scanning.
+
+## Installation
+1. Install Python (if not already installed) from [python.org](https://www.python.org/downloads/).
+2. Install required Python packages using pip:
+    ```
+    pip install aiohttp aiofiles requests fake_useragent beautifulsoup4 selenium chromedriver-autoinstaller colorama
+    ```
+3. Install Chrome WebDriver using chromedriver-autoinstaller:
+    ```python
+    import chromedriver_autoinstaller
+    chromedriver_autoinstaller.install()
+    ```
+4. Clone or download this repository to your local machine.
+
+## Usage
+1. Open a terminal or command prompt.
+2. Navigate to the directory where the toolkit is located.
+3. Run the `main.py` script:
+    ```
+    python main.py
+    ```
+4. Choose the tool you want to use by entering the corresponding number:
+    - 1: WordPress Enumeration
+    - 2: XSS Search
+    - 3: SQL Injection Scanner
+    - 4: Port Scanner
+5. Follow the prompts and input any required information.
+
+### WordPress Enumeration
+- Enter the website URL (with https://).
+- Follow the prompts to complete the enumeration process.
+
+### XSS Search
+- Enter the URL with the parameter `{xss}`.
+- Enter the path to the payloads file containing XSS payloads.
+
+### SQL Injection Scanner
+- Enter the URL to be checked.
+- Enter the path to the payload file containing SQL injection payloads.
+
+### Port Scanner
+- Enter the target IP address or domain name.
+- Enter the start and end port numbers to scan.
+
+## Contributors
+- [Gagan]
+
+---
+
+You can customize the README further based on your preferences and add any additional information or instructions as needed.
--- tool.py
+++ tool.py
@@ -1,125 +1,202 @@
-# Importing necessary libraries
-import socket
+import asyncio
+import aiohttp
+import aiofiles
 import requests
 import json
 import re
 import time
-from threading import Thread, Lock
-from queue import Queue
+import warnings
+from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 from colorama import Fore, init
-from random_user_agent.user_agent import UserAgent
-from random_user_agent.params import SoftwareName, OperatingSystem
-from rich.console import Console
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import warnings
-import argparse
-import sys
-from datetime import datetime
+import chromedriver_autoinstaller
+from urllib.parse import urljoin
+import socket
+import threading
 
-# Initialization
 init()
 Dgreen = Fore.LIGHTGREEN_EX
 Lgreen = Fore.LIGHTGREEN_EX
 Lyellw = Fore.LIGHTYELLOW_EX
 Lred = Fore.LIGHTRED_EX
 Lcyan = Fore.LIGHTCYAN_EX
-print_lock = Lock()
 
-# HolyScan global variables
-console = Console()
-N_Thread = 200
-q = Queue()
+# Call chromedriver_autoinstaller.install() before using Selenium
+chromedriver_autoinstaller.install()
 
-#banner
-def banner():
-    console.print(r"""
-    ___       _                             
-  / _ \  ___| |_ __ _ _ __   __ _ _ __ ___ 
- | | | |/ __| __/ _` | '_ \ / _` | '__/ __|
- | |_| | (__| || (_| | |_) | (_| | |  \__ \
-  \___/ \___|\__\__,_| .__/ \__,_|_|  |___/
-                     |_|                   
-""")
-# Website Scanner
-def user_finder(new_u):
+
+async def user_finder(new_u):
     new_url2 = new_u + '/wp-json/wp/v2/users'
-    user_agent_rotator = UserAgent(software_names=[SoftwareName.CHROME.value], operating_systems=[OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value], limit=100)
-    headers = {"User-Agent": user_agent_rotator.get_random_user_agent()}
+    headers = {"user-agent": UserAgent().random}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(new_url2, headers=headers) as response:
+            if response.status == 200:
+                print(Dgreen + '\n[+] Enumerating usernames : \n')
+                time.sleep(1.3)
+                data = await response.json()
+                for info in data:
+                    print(Lgreen + ' [*] Username Found : {}'.format(info['slug']))
+                    await asyncio.sleep(0.2)
+            else:
+                print(Lyellw + '\n[-] Usernames Not Found ')
 
-    r2 = requests.get(new_url2, headers=headers)
 
-    if r2.status_code == 200:
-        print(Dgreen + '\n[+] Enumerating usernames : \n')
-        time.sleep(1.3)
-        data = json.loads(r2.text)
-        for info in data:
-            print(Lgreen + ' [*] Username Found : {}'.format(info['slug']))
-            time.sleep(0.2)
-    else:
-        print(Lyellw + '\n[-] Usernames Not Found ')
+async def adminpanel_finder(org_url):
+    urlA = org_url + '/wp-login.php?action=lostpassword&error=invalidkey'
+    uagent = {"user-agent": UserAgent().random}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(urlA, headers=uagent) as response:
+            if response.status == 200:
+                r3data = await response.text()
+                pagesoup = BeautifulSoup(r3data, 'html.parser')
+                ptag = pagesoup.findAll("p", {"id": "nav"})
+                if len(ptag) > 0:
+                    for ptags in ptag:
+                        for atags in ptags.find_all('a'):
+                            if 'Log in' in atags:
+                                admin_url = atags['href']
+                            else:
+                                print(Lyellw + '\n[-] Admin panel not found ')
+                    print(Lgreen + '\n[+] Admin panel found - ', admin_url)
+                else:
+                    print(Lyellw + '\n[-] Admin panel not found ')
+            else:
+                print(Lyellw + '\n[-] Admin panel not found ')
 
-# XSS Scanner
-def xss_scanner(target, p):
+
+async def xss_search():
+    # Prompt the user to input the URL and payloads file path
+    url = input("Enter the URL with parameter as ={xss}: ")
+    payloads_file = input("Enter the path to the payloads file: ")
+
+    # Starting XSSearch
+    print("[*] Starting XSSearch ...")
+
+    # Configuring options for Chrome WebDriver
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-xss-auditor')
     options.add_argument('--disable-web-security')
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', chrome_options=options)
-    print("\n[*] Starting XSSearch ...")
-    for payload in open(p, 'r').readlines():
-        url = target.replace('{xss}', payload)
-        driver.get(url)
-        try:
-            WebDriverWait(driver, 1).until(EC.alert_is_present())
-            alert = driver.switch_to.alert
-            alert.accept()
-            print("\033[31m[+] XSS Triggered !\033[0m", payload)
-        except TimeoutException:
-            print("\033[36m[+] XSS not Triggered ! \033[0m", payload)
+
+    # Launching Chrome WebDriver
+    driver = webdriver.Chrome(options=options)
+
+    # Executing a loop for checking valid XSS payload in the given URL
+    tasks = []
+    for payload in open(payloads_file, 'r', encoding='utf-8').readlines():
+        task = asyncio.create_task(check_payload(url, payloads_file, payload, driver))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+    # Closing Chrome WebDriver
     driver.close()
 
-# Port Scanner
-def portscan(port):
+
+async def check_payload(url, payloads_file, payload, driver):
+    url_with_payload = url.replace('{xss}', payload.strip())
+    driver.get(url_with_payload)
+
     try:
-        s = socket.socket()
-        s.connect((host, port))
-    except:
-        with print_lock:
-            pass
-    else:
-        with print_lock:
-            serviceName = socket.getservbyport(port)
-            service = serviceName.upper()
-            console.print(f"\t- {service}: {port}")
-    finally:
-        s.close()
+        WebDriverWait(driver, 1).until(EC.alert_is_present())
+        alert = driver.switch_to.alert
+        alert.accept()
+        print("\033[31m[+] XSS Triggered !\033[0m", payload.strip())
+    except TimeoutException:
+        print("\033[36m[+] XSS not Triggered ! \033[0m", payload.strip())
 
-def scan_thread():
-    global q
-    while True:
-        worker = q.get()
-        portscan(worker)
-        q.task_done()
 
-def scanner(host, ports):
-    global q
-    for t in range(N_Thread):
-        t = Thread(target=scan_thread)
-        t.daemon = True
-        t.start()
-    for worker in ports:
-        q.put(worker)
-    q.join()
+# Define the get_forms function here
+def get_forms(url):
+    s = requests.Session()
+    s.headers["User-Agent"] = UserAgent().random
+    response = s.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    return soup.find_all("form")
 
-# Main function
-if __name__ == '__main__':
-    banner()
+
+def form_details(form):
+    detailsOfForm = {}
+    action = form.attrs.get("action")
+    method = form.attrs.get("method", "get")
+    inputs = []
+
+    for input_tag in form.find_all("input"):
+        input_type = input_tag.attrs.get("type", "text")
+        input_name = input_tag.attrs.get("name")
+        input_value = input_tag.attrs.get("value", "")
+        inputs.append({
+            "type": input_type,
+            "name": input_name,
+            "value": input_value,
+        })
+
+    detailsOfForm['action'] = action
+    detailsOfForm['method'] = method
+    detailsOfForm['inputs'] = inputs
+    return detailsOfForm
+
+
+def vulnerable(response):
+    errors = {"quoted string not properly terminated",
+              "unclosed quotation mark after the character string",
+              "you have an error in your SQL syntax"
+              }
+    for error in errors:
+        if error in response.content.decode().lower():
+            return True
+    return False
+
+
+def run_sql_injection_scan():
+    url = input("Enter the URL to be checked: ")
+    payload_file = input("Enter the path to the payload file: ")
+
+    try:
+        with open(payload_file, 'r', encoding='utf-8') as file:
+            payloads = file.readlines()
+    except OSError as e:
+        print("Error:", e)
+        return
+
+    warnings.filterwarnings('ignore')
+    s = requests.Session()
+    s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+
+    forms = get_forms(url)
+    print(f"[+] Detected {len(forms)} forms on {url}.")
+
+    for form in forms:
+        details = form_details(form)
+
+        for payload in payloads:
+            data = {}
+            for input_tag in details["inputs"]:
+                if input_tag["type"] == "hidden" or input_tag["value"]:
+                    data[input_tag['name']] = input_tag["value"] + payload.strip()
+                elif input_tag["type"] != "submit":
+                    data[input_tag['name']] = f"test{payload.strip()}"
+
+            if details["method"] == "post":
+                res = s.post(url, data=data)
+            elif details["method"] == "get":
+                res = s.get(url, params=data)
+
+            if vulnerable(res):
+                print("SQL injection attack vulnerability in link:", url)
+                break  # Exit the loop once vulnerability is found
+        else:
+            print("No SQL injection attack vulnerability detected")
+            break  # Exit the loop if no vulnerability found after trying all payloads
+
+
+async def run_wordpress_enumeration():
     print(Dgreen + '\nWebsite Url (with https://) : ' + Lgreen, end="")
     url = input('')
     org_url = url
@@ -127,7 +204,9 @@ if __name__ == '__main__':
     feedurl = url + '/feed'
     rssurl = url + '/rss'
     url = url + '/wp-json'
-    headers = {"user-agent": UserAgent().get_random_user_agent()}
+
+    headers = {"user-agent": UserAgent().random}
+
     try:
         testreq = requests.get(org_url, headers=headers)
     except Exception as e:
@@ -135,11 +214,13 @@ if __name__ == '__main__':
     else:
         r = requests.get(url, headers=headers)
         rcode = r.status_code
+
         if rcode == 200:
             print(Dgreen + '\nWebsite status : ', Lgreen + 'Up')
             robotres = requests.get(roboturl, headers=headers)
             feedTXT = requests.get(feedurl, headers=headers).text
             rssTXT = requests.get(rssurl, headers=headers).text
+
             if 'wp-admin' in robotres.text or 'wordpress.org' in feedTXT or 'wordpress.org' in rssTXT:
                 print(Dgreen + '\n[+] WordPress Detection : ', Lgreen + 'Yes')
                 feedres = requests.get(feedurl, headers=headers)
@@ -162,13 +243,15 @@ if __name__ == '__main__':
                     else:
                         print(Lyellw + '[-] WordPress version : Not Found !')
                 time.sleep(0.8)
+
                 data = json.loads(r.text)
                 siteName = data['name']
                 siteDesc = data['description']
                 plugins = data['namespaces']
-                print(Dgreen + '\n[+] Website name        :', Lgreen + siteName)
+
+                print(Dgreen + '\n[+] Webite name        :', Lgreen + siteName)
                 time.sleep(0.8)
-                print(Dgreen + '\n[+] Website description :', Lgreen + siteDesc)
+                print(Dgreen + '\n[+] Webite description :', Lgreen + siteDesc)
                 time.sleep(0.8)
                 print(Dgreen + '\n[+] Enumerating Plugins :', end=' ')
                 plugins = list(set(plugins))
@@ -177,13 +260,95 @@ if __name__ == '__main__':
                     elem = (i[:i.find('/')])
                     print(Lgreen + ' [*] ', elem)
                     time.sleep(0.2)
+
                 time.sleep(1)
-                adminpanel_finder(org_url)
+                await adminpanel_finder(org_url)
                 time.sleep(1)
-                user_finder(org_url)
+                await user_finder(org_url)
+
             else:
                 print(Dgreen + '\n[+] WordPress Detection : ', Lred + 'No')
         else:
             print(Dgreen + '\nWebsite status : ', Lred + 'Down' + r.reason)
+
     print(Lcyan + '')
     input('[ Thank you for using my tool ]')
+
+
+async def run_port_scan():
+    usage = "Usage: python3 scan.py"
+
+    print("-" * 70)
+    print("Port Scanner")
+    print("-" * 70)
+
+    target = input("Enter the target IP address or domain name: ")
+    start_port = int(input("Enter the start port number: "))
+    end_port = int(input("Enter the end port number: "))
+
+    print("Scanning target", target)
+
+    open_ports = []
+
+    def scan_port(port):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)  # Set timeout for socket connection
+            conn = s.connect((target, port))
+            s.close()
+            open_ports.append(port)
+            print("Port {} is open".format(port))
+        except socket.error:
+            pass  # Port is closed
+
+    # Limit the number of concurrent threads to avoid overwhelming the target or local machine
+    MAX_THREADS = 100
+    threads = []
+
+    for port in range(start_port, end_port + 1):
+        thread = threading.Thread(target=scan_port, args=(port,))
+        threads.append(thread)
+        thread.start()
+
+        # Wait for threads to finish if the maximum number of threads is reached
+        if len(threads) >= MAX_THREADS:
+            for t in threads:
+                t.join()
+            threads = []
+
+    # Wait for remaining threads to finish
+    for t in threads:
+        t.join()
+
+    if not open_ports:
+        print("No open ports found.")
+    else:
+        print("Open ports:", open_ports)
+
+    print("Scan completed")
+
+
+
+async def main():
+    print("Choose the tool you want to use:")
+    print("1. WordPress Enumeration")
+    print("2. XSS Search")
+    print("3. SQL Injection Scanner")
+    print("4. Port Scanner")
+
+    choice = input("Enter your choice (1/2/3/4): ")
+
+    if choice == "1":
+        await run_wordpress_enumeration()
+    elif choice == "2":
+        await xss_search()
+    elif choice == "3":
+        run_sql_injection_scan()
+    elif choice == "4":
+        await run_port_scan()
+    else:
+        print("Invalid choice. Please enter '1', '2', or '3'.")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
